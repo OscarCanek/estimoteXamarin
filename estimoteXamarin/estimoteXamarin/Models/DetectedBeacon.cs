80ccb9e0557ffe0f829e80822898d258bbf6f508
Oscar Canek <oscarcanek@gmail.com>
2018-06-15 12:19:18 -0600

    Update

---
+++ estimoteXamarin/estimoteXamarin/Models/DetectedBeacon.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace estimoteXamarin.Models
+{
+    public class DetectedBeacon
+    {
+        public DetectedBeacon(string deviceId, IDictionary<string, string> attachments)
+        {
+            if (!attachments.TryGetValue("AuparBusiness", out string name))
+            {
+                name = "Undefined";
+            }
+
+            Color color = new Color();
+            if (attachments.TryGetValue("Color", out string colorString))
+            {
+                color = Color.FromHex(colorString);
+            }
+            else
+            {
+                color = Color.Default;
+            }
+
+            if (attachments.TryGetValue("uuid:major:minor", out string uuid_major_minor))
+            {
+                var info = uuid_major_minor.Split(':');
+                if (!Guid.TryParse(info[0], out Guid uuid))
+                {
+                    this.Uuid = uuid;
+                }
+
+                if (int.TryParse(info[1], out int major))
+                {
+                    this.Major = major;
+                }
+
+                if (int.TryParse(info[2], out int minor))
+                {
+                    this.Minor = minor;
+                }
+            }
+
+            this.Name = name;
+            this.Color = color;
+            this.Attachments = attachments;
+            this.DeviceId = deviceId;
+        }
+
+        public string DeviceId { get; private set; }
+        public string Name { get; private set; }
+        public IDictionary<string, string> Attachments { get; private set; }
+        public Color Color { get; private set; }
+        public Guid Uuid { get; private set; }
+        public int Major { get; private set; }
+        public int Minor { get; private set; }
+    }
+}
--- estimoteXamarin/estimoteXamarin/Models/EstimoteZoneEvent.cs
+++ estimoteXamarin/estimoteXamarin/Models/EstimoteZoneEvent.cs
@@ -5,14 +5,15 @@ namespace estimoteXamarin.Models
 {
     public class EstimoteZoneEvent
     {
-        public EstimoteZoneEvent(Beacon beacon, EstimoteZoneEventTypes eventType)
+        public EstimoteZoneEvent(DetectedBeacon beacon, EstimoteZoneEventTypes eventType)
         {
             this.Beacon = beacon;
             this.EventType = eventType;
             this.Date = DateTime.Now;
         }
 
-        public Beacon Beacon { get; private set; }
+        public string Id { get; set; }
+        public DetectedBeacon Beacon { get; private set; }
         public DateTime Date { get; private set; }
         public EstimoteZoneEventTypes EventType { get; private set; }
     }
